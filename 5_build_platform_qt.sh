6849e21e7d92cf32d6c97ef1c3bf42765b8eae6d
Kirill Grachev <pllayer@mail.ru>
2024-10-25 17:39:25 +0700

    Fix error

--- 5_build_platform_qt.sh
+++ 5_build_platform_qt.sh
@@ -3,3 +3,6 @@ cd ./ogon-platform-qt
 ./install_depends_ubuntu22.sh 
 ./build.sh 
 cd ..
+
+sudo ln -s /opt/ogon/lib/qt5/plugins/platforms/libogon.so /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms/libogon.so
+
