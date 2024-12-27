32dfebb68190f6f53f402625766e73d5e6c57eb1
Kirill Grachev <pllayer@mail.ru>
2024-12-27 15:32:52 +0300

    Change build scripts for get_apps

--- 4_1_get_apps.sh
+++ 4_1_get_apps.sh
@@ -1,4 +1,1 @@
 git clone https://github.com/kgrach/ogon-apps.git
-cd ./ogon-apps 
-./build.sh 
-cd ..
---
+++ 4_2_build_apps.sh
@@ -0,0 +1,7 @@
+cd ./ogon-apps 
+mkdir -p build && cd build
+
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/opt/ogon/ -DCMAKE_INSTALL_PREFIX=/opt/ogon ..
+make -j $(grep -c '^processor' /proc/cpuinfo) install
+
+cd ../..
