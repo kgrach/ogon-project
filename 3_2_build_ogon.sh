1743554ca3d784e3492ab6aecd43c27bc38e015f
Kirill Grachev <pllayer@mail.ru>
2024-12-27 15:32:10 +0300

    refactoring ogon build script

--- 3_2_build_ogon.sh
+++ 3_2_build_ogon.sh
@@ -1,14 +1,7 @@
 cd ./ogon
 mkdir -p build && cd build
+
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/opt/ogon -DCMAKE_PREFIX_PATH=/opt/ogon/ -DWITH_OPENH264=on ..
 make install
 
 cd ../..
-
-#cd ./ogon
-#git checkout RedOS8
-#cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/opt/ogon -DCMAKE_PREFIX_PATH=/opt/ogon/ -DWITH_OPENH264=on .
-##make install
-#./install-openh264-codec.sh 
-#./install-services.sh
-#cd ..
