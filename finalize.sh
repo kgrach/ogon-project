1b49f9675a4cda2dd320819d1475af81ff900643
Kirill Grachev <pllayer@mail.ru>
2024-12-28 11:26:58 +0300

    upd finalize.sh

--- finalize.sh
+++ finalize.sh
@@ -1,3 +1,8 @@
+sudo mkdir -p /etc/ssl/private
+sudo cp certs/ssl-cert-snakeoil.key /etc/ssl/private/
+sudo cp certs/ssl-cert-snakeoil.pem /etc/ssl/certs/
+sudo cp pam/* /etc/pam.d/
+
 sudo chown root:root -R /opt/ogon
 
 sudo systemctl enable ogon-rdp-server.service
