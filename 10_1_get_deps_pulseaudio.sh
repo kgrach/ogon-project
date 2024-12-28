3858995552d0e2d3c083e9b1fbc216b4c076da6b
Kirill Grachev <pllayer@mail.ru>
2024-12-28 13:23:47 +0700

    Change build scripts for pulseaudio

---
+++ 10_1_get_deps_pulseaudio.sh
@@ -0,0 +1,1 @@
+sudo dnf install -y libSM-devel libXtst-devel intltool libtool-ltdl-devel libcap-devel cjson-devel json-c-devel libsndfile-devel
--- 10_2_get_pulseaudio.sh
+++ 10_2_get_pulseaudio.sh
@@ -1,6 +1,4 @@
 git clone https://github.com/kgrach/ogon-pulseaudio.git
 cd ./ogon-pulseaudio
 git checkout RedOS8
-./install_depends.sh
-./build.sh
 cd ..
