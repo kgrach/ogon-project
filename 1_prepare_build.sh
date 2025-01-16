ca6dd551b8c70b09f5d0ebe02d0ca2eeaf1e00cd
Kirill Grachev <pllayer@mail.ru>
2025-01-16 14:36:45 +0700

    Upd dependency list

--- 1_prepare_build.sh
+++ 1_prepare_build.sh
@@ -6,7 +6,7 @@ sudo dnf install -y openssl-devel libXext-devel libXcursor-devel libXinerama-dev
                     qtchooser libxkbcommon-devel fontconfig-devel mtdev-devel mesa-libEGL-devel qt5-qtbase-private-devel xmlto fop.noarch libtool pixman-devel \
                     pixman-devel xorg-x11-proto-devel.noarch libXfont-devel libXfont2-devel libfontenc-devel xorg-x11-util-macros.noarch xorg-x11-font-utils xorg-x11-xtrans-devel.noarch \
                     mesa-libGL-devel xorg-x11-xkb-utils-devel binutils-devel libXcomposite-devel libXpresent-devel libXpresent libXScrnSaver libXScrnSaver-devel \
-                    libXxf86misc libXxf86misc-devel
+                    libXxf86misc libXxf86misc-devel thrift thrift-devel
 
 sudo mkdir /opt/ogon
 sudo chown ${USER}:${USER} /opt/ogon
